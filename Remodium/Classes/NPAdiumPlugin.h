a249daf1bf7627b1296a416e78068801ac41ba75
Malcolm Jarvis <malcolm@boolable.ca>
2013-04-14 21:06:35 -0700

    Base Repository

---
+++ Remodium/Classes/NPAdiumPlugin.h
@@ -0,0 +1,14 @@
+//
+//  NPAdiumPlugin.h
+//  Remodium
+//
+//  Created by Malcolm on 2013-04-14.
+//  Copyright (c) 2013 Boolable. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/AIPlugin.h>
+
+@interface NPAdiumPlugin : NSObject <AIPlugin>
+
+@end
---
+++ Remodium/Classes/NPAdiumPlugin.m
@@ -0,0 +1,23 @@
+//
+//  NPAdiumPlugin.m
+//  Remodium
+//
+//  Created by Malcolm on 2013-04-14.
+//  Copyright (c) 2013 Boolable. All rights reserved.
+//
+
+#import "NPAdiumPlugin.h"
+
+@implementation NPAdiumPlugin
+
+- (void)installPlugin
+{
+    
+}
+
+- (void)uninstallPlugin
+{
+    
+}
+
+@end
